4d40614a6361599c6d0cc246fb0d29ddc8b2c2b8
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2020-12-01 17:58:20 +0900

    コンポーネントを切り出してリファクタリング

--- app/javascript/components/home/Index.vue
+++ app/javascript/components/home/Index.vue
@@ -1,46 +1,33 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <p>{{ description }}</p>
-    <table class="contents-table">
-      <tr>
-        <th>名前</th>
-        <th>リンク</th>
-      </tr>
-      <tr v-for="outer_link in contents.outer_links" v-bind:key="outer_link.name">
-        <td>{{ outer_link.name }}</td>
-        <td>
-          <a v-bind:href="outer_link.url">{{ outer_link.text }}</a>
-        </td>
-      </tr>
-    </table>
+    <HeaderView v-bind:title="title" v-bind:description="description" />
+    <Contents v-bind:contents="contents" />
   </div>
 </template>
 
 <script>
-import { toRefs } from 'vue'
+import HeaderView from "../HeaderView.vue";
+import Contents from "./Contents.vue";
 
 export default {
+  components: {
+    HeaderView,
+    Contents
+  },
   props: {
     initialData: {
       type: Object,
       default: () => {}
     }
   },
-  setup(props) {
-    const { title, description, contents } = toRefs(props.initialData)
-    return { title, description, contents }
-  }
+  data() {
+    return {
+      title: this.initialData.title,
+      description: this.initialData.description,
+      contents: this.initialData.contents
+    }
+  },
 };
 </script>
 
-<style scoped>
-.contents-table {
-  border: 1px solid gray;
-  margin: 10px;
-}
-.contents-table th,
-.contents-table td {
-  border: 1px solid gray;
-}
-</style>
+<style scoped></style>
